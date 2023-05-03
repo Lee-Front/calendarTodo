74c7734b87bea3ac1947f0f819edcba9a5375c44
wkdrmadl3 <wkdrmadl3@naver.com>
2023-05-03 14:36:17 +0900

    gh-pages 설정 배포

---
+++ .history/src/App_20230503125442.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import CalendarPage from "./page/CalendarPage";
+import TodoPage from "./page/TodoPage";
+import Header from "./common/Header";
+import { Global } from "@emotion/react";
+import { globalStyles } from "./style";
+import styled from "@emotion/styled";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+function App() {
+  const location = useLocation();
+  return (
+    <Wrapper>
+      <Global styles={globalStyles} />
+      <Header />
+      <ContentsWrapper>
+        <TransitionGroup
+          childFactory={(child) => {
+            return React.cloneElement(child, {
+              classNames: location.state?.direction || "page-transition",
+            });
+          }}
+        >
+          <CSSTransition key={location.pathname} timeout={300}>
+            <Routes location={location}>
+              <Route path="/" element={<CalendarPage />} />
+              <Route path="/todo" element={<TodoPage />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </ContentsWrapper>
+    </Wrapper>
+  );
+}
+
+export default App;
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  //max-width: 70rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const ContentsWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
---
+++ .history/webpack.common_20230503140838.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+console.log("path: ", path);
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
