b7f1cd2e01b91d312d81a712298022ddc813d661
wkdrmadl3 <wkdrmadl3@naver.com>
2023-05-12 18:00:28 +0900

    폰트 적용을 위한 스타일 수정

--- .history/src/style_20230512175545.tsx
+++ .history/src/style_20230512175545.tsx
@@ -3,7 +3,8 @@ import { css } from "@emotion/react";
 export const globalStyles = css`
   @font-face {
     font-family: "SpoqaHanSansNeo-Medium";
-    src: url("fonts/SpoqaHanSansNeo-Medium.ttf");
+
+    src: url("/fonts/SpoqaHanSansNeo-Medium.ttf");
   }
   * {
     margin: 0;
